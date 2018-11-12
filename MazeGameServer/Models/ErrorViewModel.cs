6fceeb882d0a7878cd50b94b059e363fcff4d824
Alex Kadis <alex@kadis.me>
2018-11-12 10:27:17 -0500

    Add project files.

---
+++ MazeGameServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MazeGameServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
